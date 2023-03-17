bc5bb8ab897fd33d1665a9d51a837f5c81fcf099
Barry Ceelen <barry@multipop.org>
2023-03-17 11:17:45 +0100

    Increase action hook callback priority

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@
 namespace FunctionsFile\SortSettingsMenu;
 
 // Arrange the items in the "Settings" menu of the WordPress admin in alphabetical order.
-add_action( 'admin_menu', __NAMESPACE__ . '\sort_admin_submenu_items' );
+add_action( 'admin_menu', __NAMESPACE__ . '\sort_admin_submenu_items', 999 );
 
 /**
  * Arrange the items in the "Settings" menu of the WordPress admin in alphabetical order.
